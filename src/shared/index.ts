53f1735ec26f4afcf19ae591ca0292769ee71706
永生 <>
2021-08-15 20:43:13 +0800

    fix 解决引用错误

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -22,3 +22,28 @@ export const capitalize = (str: string) =>
  */
 export const toHandlerKey = (str: string) =>
   str ? `on${capitalize(str)}` : ``;
+
+export const hasChanged = (value: any, oldValue: any): boolean => value !== oldValue && (value === value || oldValue === oldValue)
+
+export const isString = (val: unknown): val is string => typeof val === 'string'
+
+export const isIntegerKey = (key: unknown) =>
+  isString(key) &&
+  key !== 'NaN' &&
+  key[0] !== '-' &&
+  '' + parseInt(key, 10) === key
+
+export const isMap = (val: unknown): val is Map<any, any> =>
+  toTypeString(val) === '[object Map]'
+
+export const objectToString = Object.prototype.toString
+
+export const toTypeString = (value: unknown): string =>
+  objectToString.call(value)
+
+export const isObject = (val: unknown): val is Record<any, any> => val !== null && typeof val === 'object'
+
+export const toRawType = (value: unknown): string => {
+  // extract "RawType" from strings like "[object RawType]"
+  return toTypeString(value).slice(8, -1)
+}
